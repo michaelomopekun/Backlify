685b24d8cd81827c08cb0891d6e78cdac9e5a422
omopekun michael <omopekunmichael@gmail.com>
2026-06-01 23:15:59 +0100

    Refactor drizzle.config.ts to simplify database connection handling; update package.json scripts for PostgreSQL compatibility

--- package/db/drizzle.config.ts
+++ package/db/drizzle.config.ts
@@ -1,17 +1,12 @@
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
 
-const connectionString = process.env.DATABASE_URL!;
-const url = new URL(connectionString);
+// const connectionString = process.env.DATABASE_URL!;
 
-export default defineConfig({
+export default {
   schema: './schema',
   out: './migration',
-  dialect: 'postgresql',
+  driver: 'pg',
   dbCredentials: {
-    host: url.hostname,
-    port: parseInt(url.port),
-    user: url.username,
-    password: url.password,
-    database: url.pathname.slice(1),
+    connectionString: process.env.DATABASE_URL!,
   },
-});
+} satisfies Config;
